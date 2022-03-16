429fcfd837a2e0dc87d35693f2f5a520326a133b
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-03-16 18:25:03 +0100

    refactor, new name, add new test

--- LeapYearKata/LeapYearShould.cs
+++ LeapYearKata/LeapYearShould.cs
@@ -7,10 +7,14 @@ namespace LeapYearKata.Test {
         }
 
         [Test]
-        //Los años no divisibles por 4 no son bisiestos    
-        public void return_false_if_is_not_divisible_by_4() {       
+        [TestCase(4)]
+        [TestCase(8)]
+        [TestCase(12)]
+        [TestCase(16)]
+        //Los años divisibles por 4 son bisiestos    
+        public void be_divisible_by_4(int year) {       
             var leapYear = new LeapYear();
-            Assert.AreEqual(false, leapYear.IsDivisibleBy4(4));
+            Assert.AreEqual(false, leapYear.IsDivisibleBy4(year));
         }
     }
 }
