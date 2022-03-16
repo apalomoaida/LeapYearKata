f9f3db9238267874a12f72add6d7e0e71a8f458a
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-03-16 18:17:00 +0100

    green: return_false_if_is_not_divisible_by_4

---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYearKata {
+    public class LeapYear {
+        public LeapYear() {
+
+        }
+        public bool IsDivisibleBy4(int year) {
+            return false;
+        }
+    }
+}
