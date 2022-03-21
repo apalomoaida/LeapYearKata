a98148dd2b8cc26431143e41e31d3cbe2fc8c283
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-03-21 16:31:43 +0100

    refactor

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace LeapYearKata {
     public class LeapYear {
-        public LeapYear() {
-
+        private readonly int year;
+        public LeapYear(int year) {
+            this.year = year;
         }
-        public bool IsDivisibleBy4(int year) {
+        public bool IsDivisibleBy4() {
             return year % 4 == 0;
         }
     }
--- LeapYearKata/LeapYearShould.cs
+++ LeapYearKata/LeapYearShould.cs
@@ -2,8 +2,10 @@ using NUnit.Framework;
 
 namespace LeapYearKata.Test {
     public class LeapYearShould {
+       
         [SetUp]
         public void Setup() {
+            
         }
 
         #region Los años divisibles por 4 son bisiestos  
@@ -13,9 +15,9 @@ namespace LeapYearKata.Test {
         [TestCase(12)]
         [TestCase(16)]      
         public void return_true_if_is_divisible_by_4(int year) {       
-            var leapYear = new LeapYear();
+            var leapYear = new LeapYear(year);
 
-            Assert.AreEqual(true, leapYear.IsDivisibleBy4(year));
+            Assert.AreEqual(true, leapYear.IsDivisibleBy4());
         }  
         [Test]
         [TestCase(5)]
@@ -23,9 +25,9 @@ namespace LeapYearKata.Test {
         [TestCase(13)]
         [TestCase(17)]
         public void return_false_if_is_not_divisible_by_4(int year) {       
-            var leapYear = new LeapYear();
+            var leapYear = new LeapYear(year);
 
-            Assert.AreEqual(false, leapYear.IsDivisibleBy4(year));
+            Assert.AreEqual(false, leapYear.IsDivisibleBy4());
         }
         #endregion
     }
